233e88c2028739c08830baef452d27770f7ce88d
chriswo92 <chrisworkingonline@gmail.com>
2024-10-21 08:02:51 -0300

    ls App

--- 6.ls.js
+++ 6.ls.js
@@ -1,4 +1,19 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
+
+fs.readdir('.')
+    .then(files => {
+        files.forEach(file => {
+            console.log(file)
+        })
+    })
+    .catch(err => {
+        if(err) {
+            console.error('Error al leer el directorio: ', err)
+            return
+        }
+    })
+
+/* const fs = require('node:fs')
 
 fs.readdir('.', (err, files) => {
     if (err) {
@@ -9,4 +24,4 @@ fs.readdir('.', (err, files) => {
     files.forEach(file => {
         console.log(file)
     })
-})
+}) */
---
+++ 7.process.js
@@ -0,0 +1,16 @@
+// argumentos de entrada
+// console.log(process.argv)
+
+// constrolar proceso y su salida
+/* process.exit(1)
+
+process.on('exit', () => {
+    //
+})
+ */
+// current working directory
+console.log(process.cwd())
+
+// platform
+console.log(process.env.PEPITO)
+
