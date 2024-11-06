ea64ce7583584606a184818b8afbd2aefb800e6e
chriswo92 <chrisworkingonline@gmail.com>
2024-11-06 18:29:45 -0300

    server status codes and reading image

--- class-2/1.http.js
+++ class-2/1.http.js
@@ -1,4 +1,5 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
@@ -7,13 +8,23 @@ const processRequest = (req, res) => {
 
     if (req.url == '/') {
         res.statusCode = 200
-        res.end('<h1>Bienvenido a mi página de inicio<h1>')
+        res.end('<h1>Mi página</h1>')
     } else if (req.url == '/contacto') {
         res.statusCode = 200
-        res.end('<h1>Contacto<h1>')
+        res.end('<h1>Contacto</h1>')
+    } else if (req.url == '/cave.png') {
+        fs.readFile('./cave.png', (err, data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('<h1>500 Internal Server Error</h1>')
+            } else {
+                res.setHeader('Content-Type', 'image/png')
+                res.end(data)
+            }
+        })
     } else {
         res.statusCode = 404
-        res.end('<h1>404<h1>')
+        res.end('<h1>404</h1>')
     }
 }
 
@@ -21,4 +32,4 @@ const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
     console.log(`server listening on port http://localhost:${desiredPort}`)
-})
+}) 
