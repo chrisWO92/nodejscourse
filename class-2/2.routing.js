bba284c5b253582c235348313a6f55fd63892695
chriswo92 <chrisworkingonline@gmail.com>
2024-11-06 21:33:55 -0300

    routing-1

---
+++ class-2/2.routing.js
@@ -0,0 +1,26 @@
+const http = require('node:http')
+
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+    const { method, url } = req
+
+    switch (method) {
+        case 'GET':
+            switch (url) {
+                case '/pokemon/ditto':
+                    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+                    return res.end(JSON.stringify(dittoJSON))
+                default:
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+                    return res.end('<h1>404</h1>')
+            }
+    }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+    console.log('server listening on port http://localhost:1234')
+})
