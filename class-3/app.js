a94bfe080d23805f9e9e1ca48a98dc8c8d0e05e2
chriswo92 <chrisworkingonline@gmail.com>
2024-11-10 18:29:10 -0300

    implementando cors origin

--- class-3/app.js
+++ class-3/app.js
@@ -1,15 +1,41 @@
 const express = require('express')
+const cors = require('cors')
 const crypto = require('node:crypto')
 const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
+
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'http://movies.com',
+            'http://midu.dev'
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}
+))
+
 app.use(express.json()) // middleware para poder recibir con req.body lo que enviemos en el body
 app.disable('x-powered-by')
 
 app.get('/', (req, res) => {
     res.json({ message: 'hola mundo' })
 })
+
+
 
 // Todos los recursos que sean movies se identifican con /movies
 app.get('/movies', (req, res) => {
@@ -50,6 +76,19 @@ app.post('/movies', (req, res) => {
     res.status(201).json(newMovie) // Actualizar caché del cliente
 })
 
+app.delete('/movies/:id', (req, res) => {
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+
+    if (movieIndex === -1) {
+        return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    movies.splice(movieIndex, 1)
+
+    return res.json({ message: 'Movie deleted' })
+})
+
 app.patch('/movies/:id', (req, res) => {
     const result = validatePartialMovie(req.body)
 
@@ -79,6 +118,7 @@ app.patch('/movies/:id', (req, res) => {
     return res.json(updateMovie)
 
 })
+
 
 const PORT = process.env.PORT ?? 1234
 
